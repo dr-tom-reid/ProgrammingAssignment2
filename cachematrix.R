40ee53768d67df827ec0a116886c9763f072c82c
dr-tom-reid <thomasr@usca.edu>
2015-09-21 16:12:46 -0400

    My solution to the cached matrix problem

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions create a special "matrix" object and 
+## find (and cache) it's inverse.
+## Usage:
+##      A <- matrix(c(2,0,0,4),nrow=2,ncol=2)
+##      my_x <- makeCacheMatrix(A)
+##      cacheSolve(my_x)
+## (Note: subsequent calls just recall the stored inverse from cache)
 
-## Write a short comment describing this function
-
+## creates a special "matrix" object that can cache its inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+    CMinv <- NULL
+    set <- function(mx) {
+        x <<- mx
+        CMinv <<- NULL
+    }
+    get <- function() x
+    setinv <- function(mtxinv) CMinv <<- mtxinv
+    getinv <- function() CMinv
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
 
-## Write a short comment describing this function
-
+## computes the inverse of the special "matrix" returned by `makeCacheMatrix`
+## If the inverse has already been calculated (and the matrix has not changed), 
+## then `cacheSolve` should retrieve the inverse from the cache.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    mxinv <- x$getinv()
+    if(!is.null(mxinv)) {
+        message("getting cached data")
+    } else {
+    mx <- x$get()
+    mxinv <- solve(mx, ...)
+    x$setinv(mxinv)
+    }
+    mxinv
 }
